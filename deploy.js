d3e8b567786b32eccf8060980972507663b86399
MetaSolipsism <83084103+MetaSolipsism@users.noreply.github.com>
2021-09-12 10:58:12 -0500

    Update deploy.js

--- deploy.js
+++ deploy.js
@@ -91,7 +91,7 @@ function sshConnect() {
   ssh
     .connect({
       // TODO: ADD YOUR IP ADDRESS BELOW (e.g. '12.34.5.67')
-      host: '54.87.166.29',
+      host: '35.175.139.116',
       username: 'ubuntu',
       privateKey: 'hs-key.pem',
     })
